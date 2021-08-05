ad4d3af6c4dc843ebde73cfd76b4123011c53910
Leandro-SI <llmanutencao@bol.com.br>
2021-08-05 11:26:09 -0300

    class controle de usuario

---
+++ SistemaLogin/SistemaLogin/CadastroUsuarios.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaLogin
+{
+    public static class CadastroUsuarios
+    {
+        private static Usuario[] usuarios =
+        {
+            new Usuario("leandro", "123"),  
+            new Usuario("luciana", "321")
+        
+        };
+
+        private static Usuario _userLogado = null;
+
+        public static Usuario UsuarioLogado
+        {
+            get { return _userLogado; }
+            private set { _userLogado = value; }
+        }
+
+        public static bool Login(string nome, string senha)
+        {
+            foreach(Usuario user in usuarios)
+            {
+                if (user.Nome.Equals(nome) && user.Senha.Equals(senha))
+                {
+                    UsuarioLogado = user;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ SistemaLogin/SistemaLogin/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaLogin
+{
+    public class Usuario
+    {
+        public Usuario(string nome, string senha)
+        {
+            Nome = nome;
+            Senha = senha;
+        }
+
+        public Usuario() { }
+
+        public string Nome { get; set; }
+        public string Senha { get; set; }
+    }
+}
